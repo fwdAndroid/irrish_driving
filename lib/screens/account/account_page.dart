e3044a1ea3d2ea7f879806c742c65f8eff0b3255
fawad kaleem <fwdkaleem@gmail.com>
2023-07-11 02:01:02 +0500

    Main Account page Ready

--- lib/screens/account/account_page.dart
+++ lib/screens/account/account_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:irrish_driving/widgets/buttons.dart';
+import 'package:irrish_driving/widgets/colors.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -10,6 +12,82 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: colorWhite,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Image.asset("assets/logo.png"),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Login",
+              style: TextStyle(
+                color: mainColor,
+                fontSize: 30,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Please select below option to continue.",
+              style: TextStyle(
+                color: colorDark,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: AuthButton(
+                onPressed: () async {
+                  // await AuthServices().signInWithGoogle().then((value) => {
+                  //       Navigator.pushReplacement(context,
+                  //           MaterialPageRoute(builder: (builder) => CheckStatus()))
+                  //     });
+                },
+                title: "Log In With Google",
+                image: "assets/googl.png",
+              ),
+            ),
+          ),
+          AuthButton(
+            onPressed: () async {
+              // Navigator.push(
+              //     context,
+              //     MaterialPageRoute(
+              //         builder: (builder) => const ContinuePhone()));
+            },
+            title: "Log In With Phone Number",
+            image: "assets/imgGgphone.png",
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xff246A73).withOpacity(.10),
+            ),
+            margin: const EdgeInsets.only(left: 10, top: 24, right: 10),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "By registering or creating an account, you agree to our Terms of Use. Read our Privacy Policy to learn more about how we process your data.",
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xff246A73),
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "ProximaNova"),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/colors.dart
+++ lib/widgets/colors.dart
@@ -4,4 +4,5 @@ const colorWhite = Colors.white;
 const colorGreen = Colors.green;
 const colorBlack = Colors.black;
 const mainColor = Color(0xff00A79D);
+const colorDark = Color(0xFF736F7F);
 const redColor = Colors.red;
