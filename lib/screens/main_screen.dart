cfaa32b1dbb6d3995e326a9fded2d4a84818afe4
fawad kaleem <fwdkaleem@gmail.com>
2023-07-12 14:06:56 +0500

    Main Screen UI

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,15 +1,134 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:irrish_driving/screens/dashboard/booking_page.dart';
+import 'package:irrish_driving/screens/dashboard/home_page.dart';
+import 'package:irrish_driving/screens/dashboard/my_profile.dart';
+import 'package:irrish_driving/screens/dashboard/order_page.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+  // MainScreen ({Key key}) : super(key: key);
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
+  late Animation<double> fabAnimation;
+  late Animation<double> borderRadiusAnimation;
+  late CurvedAnimation fabCurve;
+  late CurvedAnimation borderRadiusCurve;
+
+  int currentTab = 0; // to keep track of active tab index
+  final List<Widget> screens = [
+    const HomePage(),
+  ]; // to store nested tabs
+  final PageStorageBucket bucket = PageStorageBucket();
+  Widget currentScreen = const HomePage(); // Our first view in viewport
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: PageStorage(
+        bucket: bucket,
+        child: currentScreen,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: SizedBox(
+          height: 40,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              //Welcome
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        const HomePage(); // if user taps on this dashboard tab will be active
+                    currentTab = 0;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      currentTab == 0
+                          ? 'assets/locationcolor.png'
+                          : 'assets/location.png',
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+
+              //Calender
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen =
+                        BookingPage(); // if user taps on this dashboard tab will be active
+
+                    currentTab = 1;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      currentTab == 1
+                          ? 'assets/colors.png'
+                          : 'assets/calender.png',
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+              //Chat
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen = const OrderPage();
+
+                    currentTab = 2;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      currentTab == 2 ? 'assets/chats.png' : 'assets/chat1.png',
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+              //Profile
+              MaterialButton(
+                minWidth: 40,
+                onPressed: () {
+                  setState(() {
+                    currentScreen = const MyProfile();
+                    currentTab = 3;
+                  });
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      currentTab == 3
+                          ? 'assets/profilecolor.png'
+                          : 'assets/person.png',
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
